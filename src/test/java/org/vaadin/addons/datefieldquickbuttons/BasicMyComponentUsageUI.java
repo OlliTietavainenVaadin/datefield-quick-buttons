a9cf478f4fc234b40ec285c6cfc469748c5d4ac6
Olli Tietäväinen <ollit@vaadin.com>
2017-07-31 11:06:37 +0300

    updated .gitignore, improved example

--- src/test/java/org/vaadin/addons/datefieldquickbuttons/BasicMyComponentUsageUI.java
+++ src/test/java/org/vaadin/addons/datefieldquickbuttons/BasicMyComponentUsageUI.java
@@ -1,7 +1,5 @@
 package org.vaadin.addons.datefieldquickbuttons;
 
-import com.vaadin.shared.Registration;
-import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.DateField;
 import org.vaadin.addonhelpers.AbstractTest;
@@ -9,18 +7,17 @@ import org.vaadin.addonhelpers.AbstractTest;
 import java.time.LocalDate;
 
 /**
- * Add many of these with different configurations,
- * combine with different components, for regressions
- * and also make them dynamic if needed.
+ * Add many of these with different configurations, combine with different components, for regressions and also make them dynamic if needed.
  */
 public class BasicMyComponentUsageUI extends AbstractTest {
 
     @Override
     public Component getTestComponent() {
         DateField df = new DateField();
-        DateFieldQuickButtonsExtension e = new DateFieldQuickButtonsExtension(() -> df.setValue(LocalDate.now()),
-            () -> df.setValue(null));
+
+        DateFieldQuickButtonsExtension e = new DateFieldQuickButtonsExtension(() -> df.setValue(LocalDate.now()), () -> df.setValue(null));
         e.setButtonCaptions("Now", "Reset");
+        e.setButtonClassNames("quick-button-1", "quick-button-2");
         e.extend(df);
         return df;
     }
